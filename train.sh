06b405f1fffa87e6726291101956d2206142e8e9
pabsan-0 <psantana@catec.aero>
2022-06-28 13:08:35 +0200

    missing fix to handle data w/ symlinks

--- train.sh
+++ train.sh
@@ -1,11 +1,11 @@
 
 # Assert dataset paths
-sed "s:images/:$PWD/robot2022/images/:g" robot2022/dataset.yaml > tmp/dataset.yaml
+sed "s:images/:$PWD/dataset/images/:g" dataset/dataset.yaml > tmp/dataset.yaml
 
 cd PyTorch_YOLOv4
     python3 train.py                        \
         --device 0                          \
-        --batch-size 2                      \
+        --batch-size 4                      \
         --img 1280                          \
         --data ../tmp/dataset.yaml    \
         --cfg ../cfg/tiny.cfg               \
